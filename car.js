e098f81e936e85656faf2a7b904783b0fc616411
Hudson512 <hmateque@gmail.com>
2025-06-09 10:26:40 +0100

    primeiro commit

---
+++ car.js
@@ -0,0 +1,72 @@
+class Car {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.color = 'blue'; // Default color
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.friction = 0.05;
+        this.maxSpeed = 3;
+        this.angle = 0;
+        this.controls = new Controls();
+    }
+
+    update() {
+        // Update position based on speed
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
+        }
+        if (this.controls.reverse) {
+            this.speed -= this.acceleration;
+        }
+        // Limit the speed
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed) {
+            this.speed = -this.maxSpeed/2;
+        }
+
+        // Apply friction
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        } else if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+
+        // Stop the car if speed is very low
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+        if (this.speed !== 0) {
+            const direction = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
+                this.angle += 0.03 * direction;
+            }
+            if (this.controls.right) {
+                this.angle -= 0.03 * direction;
+            }
+        }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+        ctx.beginPath();
+        ctx.rect(
+            -this.width/2, 
+            -this.height/2, 
+            this.width, 
+            this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill()
+        ctx.restore();
+    }
+}
---
+++ main.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById('myCanvas');
+//canvas.height = window.innerHeight;
+canvas.width = 200;
+
+const ctx = canvas.getContext('2d');
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+
+animate();
+
+function animate() {
+    canvas.height = window.innerHeight;
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    car.update();
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
